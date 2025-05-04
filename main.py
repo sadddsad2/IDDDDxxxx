a3a57bd4838ff35a0f12f067dce8a3185a4a24af
sadddsad2 <155828224+sadddsad2@users.noreply.github.com>
2025-05-04 22:45:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def run(playwright: Playwright) -> None:
         return
     
     try:
-        browser = playwright.firefox.launch(headless=True)
+        browser = playwright.firefox.launch(headless=False)
         context = browser.new_context()
         
         # 尝试加载已保存的 cookies
